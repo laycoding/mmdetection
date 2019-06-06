54fe922c453ce6fe8825eabac05ce3766b0245ab
zhang weidong <battle975444818@outlook.com>
2019-06-06 19:37:06 +0800

    fix iou loss get negative value

--- mmdet/core/loss/losses.py
+++ mmdet/core/loss/losses.py
@@ -267,6 +267,7 @@ def select_iou_loss(pred, target, weight, avg_factor=None):
         avg_factor = pred.size(0)
     assert pred.size(0) == target.size(0)
     target = target.clamp(min=0.)
+    pred = pred.clamp(min=1e-7)
     area_pred = (pred[:, 0] + pred[:, 2]) * (pred[:, 1] + pred[:, 3])
     area_gt = (target[:, 0] + target[:, 2]) * (target[:, 1] + target[:, 3])
     area_i = ((torch.min(pred[:, 0], target[:, 0]) +
